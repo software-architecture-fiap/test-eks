3a734b020f0ebd0977c119bea27ff442f375c2fb
Carol Br <icarolinebrasil@gmail.com>
2024-11-22 17:20:23 -0300

    Add initial Terraform configuration for AWS provider and workspace

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+
+  cloud {
+    organization = "tc_fiap"
+
+    workspaces {
+      name = "test-eks"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.77.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
